8cb450a63b1e262208872e751b15dc1c3a9895e6
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-08 21:45:42 +0000

    add to log action , reducer

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,28 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
 
 export const getLogs = () => async (dispatch) => {
   try {
